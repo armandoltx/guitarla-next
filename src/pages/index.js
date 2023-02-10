058db0717bf63af16f312b4beef68ca8cd55828d
Armando Ruiz <armando.ruiz@industrie.co>
2023-02-10 12:25:08 +1100

    244. Agregadas rutas y Link para mejorar performance de las rutas

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
-import { Inter } from '@next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <>
       <h1>Hola Mundo</h1>
+      <Link href="/nosotros">Nosotros</Link>
     </>
   )
 }
---
+++ src/pages/nosotros.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function Nosotros() {
+  return (
+    <>
+      <div>Nosotros</div>
+      <Link href="/">Inicio</Link>
+    </>
+  )
+}
