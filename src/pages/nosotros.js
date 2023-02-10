f232b946ed55891632a32bab86461a39695f7cb7
Armando Ruiz <armando.ruiz@industrie.co>
2023-02-10 13:06:08 +1100

    246. Agregar la informacion meta y title en el layout, para q sea dinamico

--- src/pages/nosotros.js
+++ src/pages/nosotros.js
@@ -4,7 +4,10 @@ import Layout from '@/components/layout'
 export default function Nosotros() {
   return (
     <>
-      <Layout>
+      <Layout
+        title={'Nosotros'}
+        description={'Sobre Nosogtros, Blog de musica, venta de guitarras y mas.'}
+      >
         <div>Nosotros</div>
         <Link href="/">Inicio</Link>
       </Layout>
