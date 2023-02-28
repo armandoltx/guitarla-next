eb7a8905c1c56d6ba471af436dd102ba4f0bff56
Armando Ruiz <armando.ruiz@industrie.co>
2023-02-28 16:02:16 +1100

    259. Agregando el css

--- src/pages/tienda.js
+++ src/pages/tienda.js
@@ -1,5 +1,6 @@
 import Layout from '@/components/layout'
 import Guitarra from '@/components/guitarra'
+import styles from '../styles/grid.module.css'
 
 export default function Tienda({guitarras}) {
   console.log(guitarras)
